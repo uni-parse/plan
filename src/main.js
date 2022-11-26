2730a301903597f8dc844cac65bfd466a2406472
theuniparse <theuniparse@gmail.com>
2022-11-26 22:11:10 +0100

    position hours

--- src/main.js
+++ src/main.js
@@ -12,22 +12,25 @@ for (let i = 1; i <= 6; i++) {
   hour.appendChild(min)
 }
 
-for (let i = 1; i <= 3; i++) {
+for (let i = 1; i <= 24; i++) {
   const h = hour.cloneNode(true)
   h.setAttribute('class', `hour nth${i}`)
-  threeHours.appendChild(h)
+  circle.appendChild(h)
 }
 for (let i = 1; i <= 8; i++) {
   const three = threeHours.cloneNode(true)
   three.setAttribute('class', `threeHours nth${i}`)
   circle.appendChild(three)
 }
-const hours = circle.querySelectorAll('.hour')
-hours.forEach((hour, i) => hour.id = `h${i + 1}`)
+//const hours = circle.querySelectorAll('.hour')
+//hours.forEach((hour, i) => hour.id = `h${i + 1}`)
 
 circle.id = 'circle'
 main.appendChild(circle)
 document.body.appendChild(main)
 
 
+for (let i = 1; i <= 24; i++) {
+
+}
 console.log('done')
